da5c3600fa1527d34a9fd07e8e732e20b8b29431
Sanwyn <santiagomusa@unisabana.edu.co>
2020-10-24 17:34:46 -0500

    Create modules to differentes figures
    -create a module to circle, triangle and rectangle with awesome front end

---
+++ src/main/java/com/santiago/Configurations/WebConfiguration.java
@@ -0,0 +1,14 @@
+package com.santiago.Configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+}
---
+++ src/main/resources/static/js/calculadora.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  //area triangulo
+  $(document).on("click", ".areaTria", function () {
+    var resul = trianguloArea($("#areaTriangulo").val(), $("#triAltura").val());
+    $("#resulArea").val(resul);
+  });
+
+  //peri triangulo
+  $(document).on("click", ".periTria", function () {
+    var resul = trianguloPerimetro(
+      $("#LadoA").val(),
+      $("#LadoB").val(),
+      $("#LadoC").val()
+    );
+    $("#periResul").val(resul);
+  });
+
+  //peri circulo
+  $(document).on("click", ".periCirc", function () {
+    var resul = perimetroCir($("#radioC").val());
+    $("#showPerimetro").val(resul);
+  });
+
+  //area circulo
+  $(document).on("click", ".areaCirc", function () {
+    var resul = circuloArea($("#radioC").val());
+    $("#showArea").val(resul);
+  });
+
+  //area rect
+  $(document).on("click", ".areaRect", function () {
+    var resul = ($("#altRec").val() * $("#baseRec").val());
+    $("#showAreaRect").val(resul);
+  });
+
+  //peri rect
+  $(document).on("click", ".periRect", function () {
+    if($("#altRec").val() == "" || $("#baseRec").val() == ""){
+      alert("Rellene ambos campos")
+    }else{
+      var resul = ( (2*$("#altRec").val()) + (2*$("#baseRec").val())  );
+      $("#showPeriRect").val(resul);
+    }
+ 
+  });
+
+
+});
+
+const trianguloArea = (a, b) => {
+  return (a * b) / 2;
+};
+
+const trianguloPerimetro = (a, b, c) => {
+  return parseInt(a) + parseInt(b) + parseInt(c);
+};
+
+const circuloArea = (r) => {
+  return Math.PI * Math.pow(r, 2);
+};
+
+const perimetroCir = (r) => {
+  return 2 * Math.PI * r;
+};
